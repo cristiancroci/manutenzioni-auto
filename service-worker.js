4303328c60bded53c4c0a95596aed23aa7a7f346
cristiancroci <info.cristiancroci@gmail.com>
2026-05-07 04:35:11 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,5 @@
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", () => clients.claim());
-
 self.addEventListener("fetch", (event) => {
   event.respondWith(fetch(event.request).catch(() => caches.match("index.html")));
 });
